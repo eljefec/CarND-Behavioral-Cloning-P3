392538325766334e76091bc918d99bbd6af73711
Jeff <jeliu@microsoft.com>
2017-02-06 22:28:56 -0800

    Add model.py with pseudocode.

---
+++ model.py
@@ -0,0 +1,21 @@
+import pickle
+import numpy as np
+import math
+
+# Fix error with TF and Keras
+import tensorflow as tf
+tf.python.control_flow_ops = tf
+
+print('Modules loaded.')
+
+# Load data.
+
+# Normalize features using Min-Max scaling between -0.5 and 0.5.
+
+# Split test data.
+
+# Shuffle training data.
+
+# Build Keras model.
+
+# Traing model.
