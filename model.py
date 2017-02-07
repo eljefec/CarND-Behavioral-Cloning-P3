f80582934f2d60a003355cacf5aecf12bfbb45b1
Jeff <jeliu@microsoft.com>
2017-02-06 23:52:13 -0800

    Load training data.

--- model.py
+++ model.py
@@ -1,3 +1,49 @@
+# Load data.
+
+# Source: http://stackoverflow.com/questions/800197/how-to-get-all-of-the-immediate-subdirectories-in-python
+import os
+def get_immediate_subdirectories(a_dir):
+    return [(os.path.join(a_dir, name)) for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+capdirs = get_immediate_subdirectories('d:\\carcapture')
+
+# https://carnd-forums.udacity.com/questions/36054925/answers/36057843
+import numpy as np
+import pandas as pd
+
+import matplotlib.image as mpimg
+
+def load_images(df):
+    X = []
+    y = []
+    for row in df.itertuples(True):
+        # print(row[1])
+        # print(row[4])
+        imgpath = row[1]
+        steering_angle = row[4]
+        img = mpimg.imread(imgpath)
+        X.append(img)
+        y.append(steering_angle)
+    X = np.array(X)
+    y = np.array(y)
+    return (X, y)
+
+for capdir in capdirs:
+    csvpath = os.path.join(capdir, 'driving_log.csv')
+    print(csvpath)
+    # contents = np.genfromtxt(csvpath, delimiter=',', skip_header=1, usecols=(1,), unpack=True, dtype=None)
+    df = pd.read_csv(csvpath)
+    # print(df)
+    (X, y) = load_images(df)
+    print('shape:', X.shape, y.shape)
+
+# Normalize features using Min-Max scaling between -0.5 and 0.5.
+
+# Split test data.
+
+# Shuffle training data.
+
 import pickle
 import numpy as np
 import math
@@ -7,14 +53,6 @@ import tensorflow as tf
 tf.python.control_flow_ops = tf
 
 print('Modules loaded.')
-
-# Load data.
-
-# Normalize features using Min-Max scaling between -0.5 and 0.5.
-
-# Split test data.
-
-# Shuffle training data.
 
 # Build Keras model.
 
