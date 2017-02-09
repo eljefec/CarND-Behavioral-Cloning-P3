Fix color channel bug. Use cv2.imread(). Simulator must feed BGR images to drive.py. Add normalization layer to model via Lambda layer. Stop normalizing data when loading and in drive.py.

drive.py
@@ -1,4 +1,3 @@
-import pre
 import argparse
 import base64
 import json
@@ -39,9 +38,8 @@ def telemetry(sid, data):
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
     transformed_image_array = image_array[None, :, :, :]
-    normalized_image = pre.normalize(transformed_image_array)
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
-    steering_angle = float(model.predict(normalized_image, batch_size=1))
+    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
     throttle = 0.2
     print(steering_angle, throttle)

load.py
@@ -1,10 +1,10 @@
 # Load data.
 
+import cv2
 import pre
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.image as mpimg
 import pickle
 from os.path import isfile
 
@@ -12,8 +12,6 @@ from os.path import isfile
 def get_immediate_subdirectories(a_dir):
     return [(os.path.join(a_dir, name)) for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
-
-# Source: https://carnd-forums.udacity.com/questions/36054925/answers/36057843
 
 def load_images(df, capdir, relative):
     X = []
@@ -23,7 +21,7 @@ def load_images(df, capdir, relative):
         steering_angle = row[4]
         if relative:
             imgpath = os.path.join(capdir, imgpath)
-        img = mpimg.imread(imgpath)
+        img = cv2.imread(imgpath)
         X.append(img)
         y.append(steering_angle)
     X = np.array(X)
@@ -39,6 +37,7 @@ def load_captures(capdirs, relative):
         csvpath = os.path.join(capdir, 'driving_log.csv')
         print(csvpath)
 
+        # Source: https://carnd-forums.udacity.com/questions/36054925/answers/36057843
         df = pd.read_csv(csvpath)
         (X, y) = load_images(df, capdir, relative)
 
@@ -55,7 +54,7 @@ def load_captures(capdirs, relative):
 
     return (X, y, csv_list)
 
-def load_normalized_data(filename, capture_root, relative):
+def load_data(filename, capture_root, relative):
     if isfile(filename):
         with open(filename, 'rb') as f:
             data = pickle.load(f)
@@ -72,13 +71,6 @@ def load_normalized_data(filename, capture_root, relative):
         capdirs = get_immediate_subdirectories(capture_root)
 
         (X, y, csv_list) = load_captures(capdirs, relative)
-
-        # Normalize features.
-        group_size = 1024
-        for i in range(0, X.shape[0], group_size):
-            X[i:i+group_size] = pre.normalize(X[i:i+group_size])
-
-        print('Normalized input.')
 
         data = dict()
         data['features'] = X

model.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 tf.python.control_flow_ops = tf
 
 # Load data.
-(X_train, y_train) = ld.load_normalized_data('udacity-train.p', 'e:\\udacity-data', True)
+(X_train, y_train) = ld.load_data('udacity-train.p', 'e:\\udacity-data', True)
 
 # Split test data.
 (X_train, X_test, y_train, y_test) = pre.split(X_train, y_train, 0.2)
@@ -16,17 +16,19 @@ tf.python.control_flow_ops = tf
 # Shuffle training data.
 (X_train, y_train) = pre.shuffle(X_train, y_train)
 
+model_name = 'nvda'
+
 # Build Keras model.
 image_shape = X_train.shape[1:]
-model = bld.build_model_nvda(image_shape)
+model = bld.build_model(model_name, image_shape)
 
-print('Built model.')
+print('Built model. [{}]'.format(model_name))
 
 # Compile and train the model here.
 model.compile(optimizer = Adam(lr = 0.0001), loss = 'mse', metrics = ['accuracy'])
 
 history = model.fit(X_train, y_train, batch_size=128, nb_epoch=10, validation_split=0.2)
 
-model.save('udacity-model.h5')
+model.save('model-{}.h5'.format(model_name))
 
 print('Saved model.')