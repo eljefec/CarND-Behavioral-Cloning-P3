cba9863a42500170f40ba2c08e1b4dce688b6e79
Jeff <jeliu@microsoft.com>
2017-02-15 22:19:24 -0800

    Extract ModelBuilder class. Extract dropout parameter. This is the model for candidate model-nvda-stride-dropout-corr0.14-drop0.2.h5.

--- build.py
+++ build.py
@@ -2,102 +2,106 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten, Lambda
 from keras.layers import Convolution2D, MaxPooling2D, AveragePooling2D, Cropping2D
 
-def build_model(name, input_shape):
-    if name == 'simple':
-        return build_model_simple(input_shape)
-    elif name == 'nvda':
-        return build_model_nvda(input_shape)
-    elif name == 'complex':
-        return build_model_complex(input_shape)
-    else:
-        raise ValueError('Invalid model name [{}]'.format(name))
+class ModelBuilder:
+    def __init__(self, dropout, input_shape):
+        self.dropout = dropout
+        self.input_shape = input_shape
 
-def build_model_simple(input_shape):
-    model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = input_shape))
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Dense(1))
-    return model
+    def build_model(self, name):
+        if name == 'simple':
+            return self.build_model_simple()
+        elif name == 'nvda':
+            return self.build_model_nvda()
+        elif name == 'complex':
+            return self.build_model_complex()
+        else:
+            raise ValueError('Invalid model name [{}]'.format(name))
 
-def add_conv(model, nb_filter, nb_row, nb_col, pool):
-    if (pool):
-        model.add(Convolution2D(nb_filter, nb_row, nb_col, subsample = (2, 2), border_mode = 'valid'))
-    else:
-        model.add(Convolution2D(nb_filter, nb_row, nb_col, border_mode = 'valid'))
-    model.add(Activation('relu'))
-    print(model.outputs)
+    def build_model_simple(self):
+        model = Sequential()
+        model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = self.input_shape))
+        model.add(Flatten())
+        model.add(Dense(128))
+        model.add(Dense(1))
+        return model
 
-def build_model_nvda(input_shape):
-    model = Sequential()
+    def add_conv(self, model, nb_filter, nb_row, nb_col, pool):
+        if (pool):
+            model.add(Convolution2D(nb_filter, nb_row, nb_col, subsample = (2, 2), border_mode = 'valid'))
+        else:
+            model.add(Convolution2D(nb_filter, nb_row, nb_col, border_mode = 'valid'))
+        model.add(Activation('relu'))
+        print(model.outputs)
 
-    # Crop out sky (50 pixels) and car hood (20 pixels).
-    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape = input_shape))
+    def build_model_nvda(self):
+        model = Sequential()
 
-    # Normalize input.
-    model.add(Lambda(lambda x: x / 255.0 - 0.5))
+        # Crop out sky (50 pixels) and car hood (20 pixels).
+        model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape = self.input_shape))
 
-    # Let this layer modify color.
-    # This layer is inspired by "Color Space Transformation Network" at https://arxiv.org/ftp/arxiv/papers/1511/1511.01064.pdf.
-    model.add(Convolution2D(3, 1, 1, border_mode = 'valid'))
+        # Normalize input.
+        model.add(Lambda(lambda x: x / 255.0 - 0.5))
 
-    add_conv(model, 24, 5, 5, pool = True)
-    add_conv(model, 36, 5, 5, pool = True)
-    add_conv(model, 48, 5, 5, pool = True)
-    add_conv(model, 64, 3, 3, pool = True)
-    add_conv(model, 64, 3, 3, pool = True)
+        # Let this layer modify color.
+        # This layer is inspired by "Color Space Transformation Network" at https://arxiv.org/ftp/arxiv/papers/1511/1511.01064.pdf.
+        model.add(Convolution2D(3, 1, 1, border_mode = 'valid'))
 
-    model.add(Flatten())
+        self.add_conv(model, 24, 5, 5, pool = True)
+        self.add_conv(model, 36, 5, 5, pool = True)
+        self.add_conv(model, 48, 5, 5, pool = True)
+        self.add_conv(model, 64, 3, 3, pool = True)
+        self.add_conv(model, 64, 3, 3, pool = True)
 
-    model.add(Dropout(0.5))
-    model.add(Dense(100, activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(50, activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation = 'relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1))
-    return model
+        model.add(Flatten())
 
-def build_model_complex(input_shape):
-    model = Sequential()
+        model.add(Dropout(self.dropout))
+        model.add(Dense(100, activation = 'relu'))
+        model.add(Dropout(self.dropout))
+        model.add(Dense(50, activation = 'relu'))
+        model.add(Dropout(self.dropout))
+        model.add(Dense(10, activation = 'relu'))
+        model.add(Dropout(self.dropout))
+        model.add(Dense(1))
+        return model
 
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = input_shape))
+    def build_model_complex(self):
+        model = Sequential()
 
-    # Let this layer modify color.
-    # This layer is inspired by "Color Space Transformation Network" at https://arxiv.org/ftp/arxiv/papers/1511/1511.01064.pdf.
-    model.add(Convolution2D(3, 1, 1,
-                            border_mode = 'valid',
-                            input_shape = input_shape))
+        model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = self.input_shape))
 
-    conv_pool_count = 3
-    for i in range(conv_pool_count):
-        model.add(Convolution2D(32, 3, 3,
+        # Let this layer modify color.
+        # This layer is inspired by "Color Space Transformation Network" at https://arxiv.org/ftp/arxiv/papers/1511/1511.01064.pdf.
+        model.add(Convolution2D(3, 1, 1,
                                 border_mode = 'valid'))
-        model.add(MaxPooling2D(pool_size = (2, 2)))
+
+        conv_pool_count = 3
+        for i in range(conv_pool_count):
+            model.add(Convolution2D(32, 3, 3,
+                                    border_mode = 'valid'))
+            model.add(MaxPooling2D(pool_size = (2, 2)))
+            model.add(Activation('relu'))
+
+        # model.add(Dropout(0.5))
+
+        conv_count = 3
+        for i in range(conv_count):
+            model.add(Convolution2D(64 * (2 ** i), 5, 5, border_mode = 'valid'))
+            model.add(Activation('relu'))
+
+        # model.add(Dropout(0.5))
+
+        print(model.outputs)
+
+        model.add(Flatten())
+
+        model.add(Dense(512))
         model.add(Activation('relu'))
+        # model.add(Dropout(0.5))
 
-    # model.add(Dropout(0.5))
-
-    conv_count = 3
-    for i in range(conv_count):
-        model.add(Convolution2D(64 * (2 ** i), 5, 5, border_mode = 'valid'))
+        model.add(Dense(512))
         model.add(Activation('relu'))
+        # model.add(Dropout(0.5))
 
-    # model.add(Dropout(0.5))
+        model.add(Dense(1))
 
-    print(model.outputs)
-
-    model.add(Flatten())
-
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    # model.add(Dropout(0.5))
-
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    # model.add(Dropout(0.5))
-
-    model.add(Dense(1))
-
-    return model
+        return model
--- model.py
+++ model.py
@@ -4,8 +4,8 @@ import pre
 import build as bld
 from keras.optimizers import Adam
 
-def train_model(model_id, capture_root, steering_correction):
-    print('Train model. id=[{}], capture_root=[{}], steering_correction=[{}]'.format(model_id, capture_root, steering_correction))
+def train_model(model_id, capture_root, steering_correction, dropout):
+    print('Train model. id=[{}], capture_root=[{}], steering_correction=[{}], dropout=[{}]'.format(model_id, capture_root, steering_correction, dropout))
 
     # Load data.
     (X_train, y_train) = ld.load_data(model_id, capture_root, steering_correction)
@@ -20,7 +20,8 @@ def train_model(model_id, capture_root, steering_correction):
 
     # Build Keras model.
     image_shape = X_train.shape[1:]
-    model = bld.build_model(model_name, image_shape)
+    model_builder = bld.ModelBuilder(dropout, image_shape)
+    model = model_builder.build_model(model_name)
 
     print('Built model. [{}]'.format(model_name))
 
@@ -35,7 +36,7 @@ def train_model(model_id, capture_root, steering_correction):
 
     history = model.fit(X_train, y_train, batch_size=128, nb_epoch=10, validation_split=0.2)
 
-    model_filename = 'model-{}-{}-corr{}.h5'.format(model_name, model_id, steering_correction)
+    model_filename = 'model-{}-{}-corr{}-drop{}.h5'.format(model_name, model_id, steering_correction, dropout)
 
     model.save(model_filename)
 
@@ -45,5 +46,6 @@ parser = argparse.ArgumentParser(description='Build Model')
 parser.add_argument('id', type=str, help='Optional id.')
 args = parser.parse_args()
 
-for steering_correction in range(10, 15):
-    train_model(args.id, 'e:\\capture-data', steering_correction / 100)
+for steering_correction in [0.10, 0.12, 0.14, 0.16]:
+    for dropout in [0.2, 0.4]:
+        train_model(args.id, 'e:\\capture-data', steering_correction, dropout)
